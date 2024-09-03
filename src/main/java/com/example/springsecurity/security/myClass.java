1946bebfda7ab19ff1f63e1e77f9d3ed1510f669
Shaktixcelore <shakti.singh@xcelore.com>
2024-09-03 11:18:42 +0530

    first commit

---
+++ src/main/java/com/example/springsecurity/security/myClass.java
@@ -0,0 +1,7 @@
+package com.example.springsecurity.security;
+
+public class myClass {
+ public static void main(String[] args) {
+    
+ }   
+}
---
+++ src/test/java/com/example/springsecurity/security/SecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springsecurity.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
